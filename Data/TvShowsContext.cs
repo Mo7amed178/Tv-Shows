90e821ec8095194c1765763202f8373cf3460e9d
Mohammed Alahmad <malahmad@Netways.com>
2019-10-06 16:57:10 +0300

    First Commit

---
+++ Data/TvShowsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TvShows.Models
+{
+    public class TvShowsContext : DbContext
+    {
+        public TvShowsContext (DbContextOptions<TvShowsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TvShows.Models.TvShow> TvShow { get; set; }
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TvShows.Models
+{
+    public enum Genre
+    {
+        Drama,
+        Comedy,
+        Romance,
+        [Display(Name = "Comedy and Romeance")]
+        ComRom,
+        Crime,
+        Mystery
+
+    }
+}
